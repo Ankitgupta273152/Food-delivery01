56473e56dfc06ce97d6931c36312a7ca4fe9f657
Ankitgupta273152 <159172118+Ankitgupta273152@users.noreply.github.com>
2026-03-01 19:42:59 +0530

    Add OwnerItemCard component to OwnerDashboard

--- frontend/src/components/OwnerDashboard.jsx
+++ frontend/src/components/OwnerDashboard.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom'
 import { FaUtensils, FaPen, FaPlus } from "react-icons/fa";
 import Footer from './Footer'
 import OwnerFoodCard from './OwnerFoodCard'
+import OwnerItemCard from './OwnerItemCard'
 import { useEffect } from 'react';
 import { setPendingOrdersCount } from '../redux/userSlice';
 
@@ -140,3 +141,4 @@ useEffect(()=>{
 }
 
 export default OwnerDashboard
+
